8cdc5c54424229dc00d9100b7848d4b0cbd69068
cheloscar <cheloscar@gmail.com>
2022-04-03 22:02:48 -0300

    Inicialización del Proyecto
    Archivos base creados por Visual Studio.

---
+++ CAI_03-EjercicioTableroV2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAI_03EjercicioTableroV2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
